a4e2f1a9aefa17c6240a6b475db99e83996a831c
Asilkhoja Mansurov <asilxojamansurov217@gmail.com>
2023-12-19 18:45:33 +0500

    Interfaces and Repositoroy Finished !!!

---
+++ src/OneApplyDataAccessLayer/Interfaces/RolesInterfaces/IAspNetUserRoleInterface.cs
@@ -0,0 +1,7 @@
+﻿using OneApplyDataAccessLayer.Entities.Roles;
+
+namespace OneApplyDataAccessLayer.Interfaces.RolesInterfaces;
+
+public interface IAspNetUserRoleInterface : IRepository<AspNetUserRole>
+{
+}
---
+++ src/OneApplyDataAccessLayer/Interfaces/VacanciesInterfaces/IApplyInterface.cs
@@ -0,0 +1,7 @@
+﻿using OneApplyDataAccessLayer.Entities.Vacancies;
+
+namespace OneApplyDataAccessLayer.Interfaces.VacanciesInterfaces;
+
+public interface IApplyInterface : IRepository<Apply>
+{
+}
