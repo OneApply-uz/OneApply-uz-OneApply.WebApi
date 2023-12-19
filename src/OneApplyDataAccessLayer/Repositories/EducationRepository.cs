28a465e9768cbed5065dd1db7b2128fee098e034
Asilkhoja Mansurov <asilxojamansurov217@gmail.com>
2023-12-19 15:11:57 +0500

    nimadir da

--- src/OneApplyDataAccessLayer/Repositories/EducationRepository.cs
+++ src/OneApplyDataAccessLayer/Repositories/EducationRepository.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace OneApplyDataAccessLayer.Repositories;
 
-namespace OneApplyDataAccessLayer.Repositories
+public class EducationRepository
 {
-    internal class EducationRepository
-    {
-    }
 }
